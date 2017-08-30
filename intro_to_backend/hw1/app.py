2a454f3a295e5f722d31b79f3823307973f0b16a
Matt Hosack <hosack.matt@gmail.com>
2017-08-30 15:00:36 -0700

    Adding application and template for homework 1

--- intro_to_backend/hw1/app.py
+++ intro_to_backend/hw1/app.py
@@ -4,16 +4,16 @@ import codecs
 app = Flask(__name__)
 
 
-@app.route("/<text>")
-def index(text):
-	text = request.args.get("text", "")
-	return render_template("rot13.html", text=text)
+@app.route("/")
+def index():
+    text = request.args.get("text", "")
+    return render_template("rot13.html", text=text)
 
 
 @app.route("/submit", methods=["POST"])
 def submit():
-	text = request.form.get("text", "")
-	return redirect(url_for("index", text=text))
+    text = codecs.encode(request.form.get("text", ""), "rot13")
+    return redirect(url_for("index", text=text))
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
