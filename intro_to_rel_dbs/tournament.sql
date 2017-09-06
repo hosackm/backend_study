b3f63538f5ae1cd6d917c8ca92b714759a6128fa
Matt Hosack <hosack.matt@gmail.com>
2017-09-05 17:10:35 -0700

    Commenting out fake database entries.  Fixed bug where matches started at 1.

--- intro_to_rel_dbs/tournament.sql
+++ intro_to_rel_dbs/tournament.sql
@@ -1,5 +1,6 @@
-DROP VIEW IF EXISTS wins;
+DROP VIEW IF EXISTS standings;
 DROP VIEW IF EXISTS totalgames;
+DROP VIEW IF EXISTS wins;
 DROP TABLE IF EXISTS matches;
 DROP TABLE IF EXISTS players;
 
@@ -16,15 +17,15 @@ CREATE TABLE matches(
     FOREIGN KEY (loser) REFERENCES players(id)
 );
 
-INSERT INTO players (name) VALUES ('Matt');     -- 1-1
-INSERT INTO players (name) VALUES ('Scott');    -- 1-1
-INSERT INTO players (name) VALUES ('Curtis');   -- 1-1
-INSERT INTO players (name) VALUES ('Wes');      -- 1-1
+-- INSERT INTO players (name) VALUES ('Matt');     -- 1-1
+-- INSERT INTO players (name) VALUES ('Scott');    -- 1-1
+-- INSERT INTO players (name) VALUES ('Curtis');   -- 1-1
+-- INSERT INTO players (name) VALUES ('Wes');      -- 1-1
 
-INSERT INTO matches (winner, loser) VALUES(1, 2); -- Matt beat Scott
-INSERT INTO matches (winner, loser) VALUES(4, 3); -- Wes beat Curtis
-INSERT INTO matches (winner, loser) VALUES(4, 1); -- Wes beat Matt
-INSERT INTO matches (winner, loser) VALUES(2, 3); -- Scott beat Curtis
+-- INSERT INTO matches (winner, loser) VALUES(1, 2); -- Matt beat Scott
+-- INSERT INTO matches (winner, loser) VALUES(4, 3); -- Wes beat Curtis
+-- INSERT INTO matches (winner, loser) VALUES(4, 1); -- Wes beat Matt
+-- INSERT INTO matches (winner, loser) VALUES(2, 3); -- Scott beat Curtis
 
 -- Create a view to track the wins a player has
 CREATE VIEW wins AS (
@@ -37,7 +38,7 @@ CREATE VIEW wins AS (
 
 -- Create a view to track total game played
 CREATE VIEW totalgames AS (
-    SELECT players.id, COUNT(players.id) as num
+    SELECT players.id, COUNT(matches.loser) as num
     FROM players
     LEFT JOIN matches
     ON players.id IN (matches.winner, matches.loser)
@@ -46,9 +47,9 @@ CREATE VIEW totalgames AS (
 
 -- Create a view to track the standings
 CREATE VIEW standings AS (
-    SELECT players.id, players.name, CAST(wins.num AS FLOAT) / CAST(totalgames.num AS FLOAT) AS pct
+    SELECT players.id, players.name, wins.num AS wins, totalgames.num AS total
     FROM players, wins, totalgames
     WHERE players.id = wins.id
     AND players.id = totalgames.id
-    ORDER BY pct DESC
+    ORDER BY CAST(wins.num AS FLOAT) / CAST(NULLIF(totalgames.num, 0) AS FLOAT) DESC
 );
