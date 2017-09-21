f69ab106582a97ad4b5f8e5f54cf79814a353760
Matt Hosack <hosack.matt@gmail.com>
2017-09-21 01:29:42 -0700

    Adding simple tests for creating tree and adding nodes

--- techinterview/data_structures/trees/go/tree_test.go
+++ techinterview/data_structures/trees/go/tree_test.go
@@ -3,5 +3,35 @@ package main
 import "testing"
 
 func TestTreeIsEmpty(t *testing.T) {
-    t.Error("Fail on purpose")
+    tree := NewTree()
+    if tree.Root != nil {
+        t.Errorf("Tree was not initialized with a nil root")
+    }
+}
+
+func TestFirstInsertBecomesRoot(t *testing.T) {
+    tree := NewTree()
+    expected := 42
+    tree.Insert(expected)
+    got := tree.Root.Value
+    if got != expected {
+        t.Errorf("Expected: ", expected, "got", got)
+    }
+}
+
+func TestInsertTwoFirstIsRootSecondIsChild(t *testing.T) {
+    tree := NewTree()
+    expected := []int{42, 20}
+
+    for _, v := range expected {
+        tree.Insert(v)
+    }
+
+    if tree.Root.Value != expected[0] {
+        t.Errorf("Expected root to be", expected[0], "got", tree.Root.Value)
+    }
+    if tree.Root.Left.Value != expected[1] {
+        t.Errorf("Expected root to be", expected[1], "got", tree.Root.Left.Value)
+    }
+
 }
